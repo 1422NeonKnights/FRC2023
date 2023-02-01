068511726c607b982dc74eaf13a8558d0a1459ec
“Error1223” <hyunsu1129.lim@gmail.com>
2023-01-31 20:12:34 -0800

    safety measures

--- src/main/java/frc/robot/commands/auto/FollowDrive.java
+++ src/main/java/frc/robot/commands/auto/FollowDrive.java
@@ -46,7 +46,9 @@ public class FollowDrive extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    driveTrain.stopMotors();
+  }
 
   // Returns true when the command should end.
   @Override
