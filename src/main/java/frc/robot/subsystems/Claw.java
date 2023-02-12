9a33af1f61318b764bc2919684644fc61641c9af
Error1223 <hyunsu1129.lim@gmail.com>
2023-02-12 10:16:05 -0800

    compressor control

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -7,25 +7,36 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Claw extends SubsystemBase {
   //pneumatics variables
-  public Compressor compressor;
-  DoubleSolenoid grabber;
+  private Compressor compressor;
+  private DoubleSolenoid grabber;
+  private boolean clawStat = false;
 
   //Smartdashboard values
   double compressorPressure;
 
   /** Creates a new Claw. */
   public Claw() {
+    compressor = new Compressor(PneumaticsModuleType.CTREPCM);
     grabber = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 4, 5);
+
+    disableCompressor();
   }
-  public double getCompressorPressure(){
-    return compressor.getPressure();
+
+  public void enableCompressor(){
+    compressor.enableDigital();
   }
+
+  public void disableCompressor(){
+    compressor.disable();
+  }
+
   public boolean isClawOpen(){
-    if(grabber.isRevSolenoidDisabled()){
+    if(clawStat){
       return true;
     }else{
       return false;
@@ -33,13 +44,19 @@ public class Claw extends SubsystemBase {
   }
   public void pitchDown(){
     grabber.set(DoubleSolenoid.Value.kReverse);
+    clawStat = false;
   }
   public void pitchUp(){
     grabber.set(DoubleSolenoid.Value.kForward);
+    clawStat = true;
   }
 
   @Override
   public void periodic() {
+    SmartDashboard.putBoolean("Compressor Status", compressor.isEnabled());
+    SmartDashboard.putBoolean("Claw Status", isClawOpen());
+    
+    SmartDashboard.putNumber("Compressor Voltage", compressor.getCurrent());
   }
   
 }
